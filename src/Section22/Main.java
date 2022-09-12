555511d36862344a24ce24ea01b40fc889e02aad
szamotine <77281802+szamotine@users.noreply.github.com>
2022-09-12 16:40:25 -0700

    Up to 327

--- src/Section22/Main.java
+++ src/Section22/Main.java
@@ -1,13 +1,20 @@
 package Section22;
 
 import Section22.Models.Pants;
-import Section22.Models.Product;
 import Section22.Models.Shirt;
 
 public class Main {
     public static void main(String[] args){
-        Product shirt = new Shirt(Shirt.Size.SMALL, 5.99, "blue", "Java Vuitton" );
-        Product pants = new Pants(32, 24.99, "blue", "Java Klein");
+        Shirt shirt = new Shirt(Shirt.Size.SMALL, 5.99, "blue", "Java Vuitton" );
+        Pants pants = new Pants(32, 24.99, "blue", "Java Klein");
+
+        Shirt shirt2 = new Shirt(shirt);
+        shirt2.setSize(Shirt.Size.MEDIUM);
+        Pants pants2 = new Pants(pants);
+
+        System.out.println(shirt);
+        System.out.println(pants);
+
     }
 }
 
--- src/Section22/Models/Shirt.java
+++ src/Section22/Models/Shirt.java
@@ -1,5 +1,7 @@
 package Section22.Models;
 
+import java.util.Objects;
+
 public class Shirt extends Product {
     public enum Size{
         SMALL,
@@ -24,5 +26,37 @@ public class Shirt extends Product {
 
     public void setSize(Size size) {
         this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        if (!(o instanceof Shirt))
+            return false;
+        Shirt shirt = (Shirt) o;
+        return size.equals(shirt.size)
+                && super.getBrand().equals(shirt.getBrand())
+                && super.getColor().equals(shirt.getColor())
+                && super.getPrice() == shirt.getPrice();
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, super.getBrand(), super.getColor(), super.getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " size= '" + size + "'" +
+                super.toString() + "}";
+    }
+
+    @Override
+    public void fold() {
+        System.out.println("Lay shirt on flat surface");
+        System.out.println("Fold shirt sideways");
+        System.out.println("Bring sleeves in");
+        System.out.println("Fold from bottom up");
     }
 }
