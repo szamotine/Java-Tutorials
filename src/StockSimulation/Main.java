7098c7587d5b0501fd4a788ee836c1f21cd29ab2
szamotine <77281802+szamotine@users.noreply.github.com>
2022-09-20 15:20:16 -0600

    Created StockSimulation file

---
+++ src/StockSimulation/Main.java
@@ -0,0 +1,8 @@
+package StockSimulation;
+
+public class Main {
+    //TODO: https://www.learnthepart.com/course/2dfda34d-6bbc-4bd5-8f45-d5999de2f514/d10bc2a7-40e9-47a2-88f4-38cb3614724a
+    public static void main (String[] args){
+        System.out.println("Stock main");
+    }
+}
